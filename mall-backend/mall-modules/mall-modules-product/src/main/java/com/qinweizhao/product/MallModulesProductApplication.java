f225b4ad39c5f6b0732978288d8c35dba2a2e624
YVKG <yvkg@qq.com>
2022-02-05 22:50:15 +0800

    commit

---
+++ mall-backend/mall-modules/mall-modules-product/src/main/java/com/qinweizhao/product/MallModulesProductApplication.java
@@ -0,0 +1,13 @@
+package com.qinweizhao.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallModulesProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallModulesProductApplication.class, args);
+    }
+
+}
---
+++ mall-backend/mall-modules/mall-modules-product/src/test/java/com/qinweizhao/product/MallModulesProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinweizhao.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallModulesProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
