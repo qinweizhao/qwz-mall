13e5cb409fadb60c1a501196444841012e9c0d67
YVKG <yvkg@qq.com>
2022-03-12 18:51:41 +0800

    commit

--- mall-backend/mall-service/mall-service-product/src/main/java/com/qinweizhao/product/ProductApplication.java
+++ mall-backend/mall-service/mall-service-product/src/main/java/com/qinweizhao/product/ProductApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MallModulesProductApplication {
+public class ProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallModulesProductApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 
 }
