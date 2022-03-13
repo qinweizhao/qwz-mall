ac061dd949aee83c6f3f76bdda29011e29b292d9
YVKG <yvkg@qq.com>
2022-03-13 17:28:05 +0800

    commit

--- mall-backend/mall-service/mall-service-activity/src/main/java/com/qinweizhao/activity/ActivityApplication.java
+++ mall-backend/mall-service/mall-service-activity/src/main/java/com/qinweizhao/activity/ActivityApplication.java
@@ -3,6 +3,9 @@ package com.qinweizhao.activity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author qinweizhao
+ */
 @SpringBootApplication
 public class ActivityApplication {
 
