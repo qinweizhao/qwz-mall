abb291322b73117ea590094cf0b52d1f36d648b4
YVKG <yvkg@qq.com>
2022-03-14 00:34:52 +0800

    commit

--- mall-backend/mall-service/mall-service-generator/src/main/java/com/qinweizhao/generator/GeneratorApplication.java
+++ mall-backend/mall-service/mall-service-generator/src/main/java/com/qinweizhao/generator/GeneratorApplication.java
@@ -18,15 +18,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(GeneratorApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  代码生成模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("代码生成模块启动成功");
     }
 }
--- mall-backend/mall-service/mall-service-generator/src/main/java/com/qinweizhao/generator/controller/GenController.java
+++ mall-backend/mall-service/mall-service-generator/src/main/java/com/qinweizhao/generator/controller/GenController.java
@@ -14,6 +14,7 @@ import com.qinweizhao.generator.service.IGenTableService;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
