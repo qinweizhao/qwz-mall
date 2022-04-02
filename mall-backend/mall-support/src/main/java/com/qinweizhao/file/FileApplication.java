0736adf4f257852a74c0e128a0a6e778a017a6ca
YVKG <yvkg@qq.com>
2022-04-03 01:43:12 +0800

    commit

--- mall-backend/mall-support/src/main/java/com/qinweizhao/file/FileApplication.java
+++ mall-backend/mall-support/src/main/java/com/qinweizhao/file/FileApplication.java
@@ -17,15 +17,6 @@ public class FileApplication
     public static void main(String[] args)
     {
         SpringApplication.run(FileApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  文件服务模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("服务支持模块启动成功");
     }
 }
