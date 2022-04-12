21457b0bff1f033b437005745c68db6fa763391b
YVKG <yvkg@qq.com>
2022-04-13 00:05:54 +0800

    commit

--- mall-backend/mall-service/mall-service-product/src/main/java/com/qinweizhao/product/controller/PmsCategoryController.java
+++ mall-backend/mall-service/mall-service-product/src/main/java/com/qinweizhao/product/controller/PmsCategoryController.java
@@ -23,6 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/category")
 public class PmsCategoryController extends BaseController {
+
     @Resource
     private IPmsCategoryService pmsCategoryService;
 
