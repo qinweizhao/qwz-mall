8c5789aec69bd567898f407e3c8210af745079f9
YVKG <yvkg@qq.com>
2022-05-01 00:04:59 +0800

    commit

--- mall-backend/mall-service/mall-service-user/src/main/java/com/qinweizhao/user/UserApplication.java
+++ mall-backend/mall-service/mall-service-user/src/main/java/com/qinweizhao/user/UserApplication.java
@@ -3,12 +3,14 @@ package com.qinweizhao.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author qinweizhao
  * @since 2021-12-22
  */
 @MapperScan("com.qinweizhao.user")
+@EnableFeignClients("com.qinweizhao")
 @SpringBootApplication
 public class UserApplication {
 
