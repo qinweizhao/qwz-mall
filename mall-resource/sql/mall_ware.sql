bd67477d8cedf7b408e7a20c4019dc8f77ec712c
YVKG <yvkg@qq.com>
2022-05-04 01:45:37 +0800

    commit

--- mall-resource/sql/mall_ware.sql
+++ mall-resource/sql/mall_ware.sql
@@ -11,168 +11,141 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 15/03/2022 16:36:30
+ Date: 04/05/2022 01:45:29
 */
 
 SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for undo_log
--- ----------------------------
-DROP TABLE IF EXISTS `undo_log`;
-CREATE TABLE `undo_log` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `branch_id` bigint NOT NULL,
-  `xid` varchar(100) NOT NULL,
-  `context` varchar(128) NOT NULL,
-  `rollback_info` longblob NOT NULL,
-  `log_status` int NOT NULL,
-  `log_created` datetime NOT NULL,
-  `log_modified` datetime NOT NULL,
-  `ext` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- ----------------------------
--- Records of undo_log
--- ----------------------------
-BEGIN;
-COMMIT;
+SET
+FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for wms_purchase
 -- ----------------------------
 DROP TABLE IF EXISTS `wms_purchase`;
-CREATE TABLE `wms_purchase` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `assignee_id` bigint DEFAULT NULL,
-  `assignee_name` varchar(255) DEFAULT NULL,
-  `phone` char(13) DEFAULT NULL,
-  `priority` int DEFAULT NULL,
-  `status` int DEFAULT NULL,
-  `ware_id` bigint DEFAULT NULL,
-  `amount` decimal(18,4) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='采购信息';
-
--- ----------------------------
--- Records of wms_purchase
--- ----------------------------
-BEGIN;
-COMMIT;
+CREATE TABLE `wms_purchase`
+(
+    `id`            bigint NOT NULL AUTO_INCREMENT COMMENT '采购单id',
+    `assignee_id`   bigint                                                        DEFAULT NULL COMMENT '采购人id\n',
+    `assignee_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '采购人名',
+    `phone`         char(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci     DEFAULT NULL COMMENT '联系方式',
+    `priority`      int                                                           DEFAULT NULL COMMENT '优先级',
+    `status`        int                                                           DEFAULT NULL COMMENT '状态',
+    `ware_id`       bigint                                                        DEFAULT NULL COMMENT '仓库id',
+    `amount`        decimal(18, 4)                                                DEFAULT NULL COMMENT '总金额',
+    `create_by`     varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '创建者',
+    `update_by`     varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '更新者',
+    `create_time`   datetime                                                      DEFAULT NULL COMMENT '创建时间',
+    `update_time`   datetime                                                      DEFAULT NULL COMMENT '更新时间',
+    `remark`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='采购单';
 
 -- ----------------------------
 -- Table structure for wms_purchase_detail
 -- ----------------------------
 DROP TABLE IF EXISTS `wms_purchase_detail`;
-CREATE TABLE `wms_purchase_detail` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `purchase_id` bigint DEFAULT NULL COMMENT '采购单id',
-  `sku_id` bigint DEFAULT NULL COMMENT '采购商品id',
-  `sku_num` int DEFAULT NULL COMMENT '采购数量',
-  `sku_price` decimal(18,4) DEFAULT NULL COMMENT '采购金额',
-  `ware_id` bigint DEFAULT NULL COMMENT '仓库id',
-  `status` int DEFAULT NULL COMMENT '状态[0新建，1已分配，2正在采购，3已完成，4采购失败]',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of wms_purchase_detail
--- ----------------------------
-BEGIN;
-COMMIT;
+CREATE TABLE `wms_purchase_detail`
+(
+    `id`          bigint NOT NULL AUTO_INCREMENT,
+    `purchase_id` bigint                                                        DEFAULT NULL COMMENT '采购单id',
+    `sku_id`      bigint                                                        DEFAULT NULL COMMENT '采购商品id',
+    `sku_num`     int                                                           DEFAULT NULL COMMENT '采购数量',
+    `sku_price`   decimal(18, 4)                                                DEFAULT NULL COMMENT '采购金额',
+    `ware_id`     bigint                                                        DEFAULT NULL COMMENT '仓库id',
+    `status`      int                                                           DEFAULT NULL COMMENT '状态[0新建，1已分配，2正在采购，3已完成，4采购失败]',
+    `create_by`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '创建者',
+    `update_by`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '更新者',
+    `create_time` datetime                                                      DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime                                                      DEFAULT NULL COMMENT '更新时间',
+    `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='采购单详情';
 
 -- ----------------------------
 -- Table structure for wms_ware_info
 -- ----------------------------
 DROP TABLE IF EXISTS `wms_ware_info`;
-CREATE TABLE `wms_ware_info` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `name` varchar(255) DEFAULT NULL COMMENT '仓库名',
-  `address` varchar(255) DEFAULT NULL COMMENT '仓库地址',
-  `areacode` varchar(20) DEFAULT NULL COMMENT '区域编码',
-  PRIMARY KEY (`id`)
+CREATE TABLE `wms_ware_info`
+(
+    `id`          bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `name`        varchar(255)                                                  DEFAULT NULL COMMENT '仓库名',
+    `address`     varchar(255)                                                  DEFAULT NULL COMMENT '仓库地址',
+    `areacode`    varchar(20)                                                   DEFAULT NULL COMMENT '区域编码',
+    `create_by`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '创建者',
+    `update_by`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '更新者',
+    `create_time` datetime                                                      DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime                                                      DEFAULT NULL COMMENT '更新时间',
+    `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
+    PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='仓库信息';
-
--- ----------------------------
--- Records of wms_ware_info
--- ----------------------------
-BEGIN;
-COMMIT;
 
 -- ----------------------------
 -- Table structure for wms_ware_order_task
 -- ----------------------------
 DROP TABLE IF EXISTS `wms_ware_order_task`;
 CREATE TABLE `wms_ware_order_task` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `order_id` bigint DEFAULT NULL COMMENT 'order_id',
-  `order_sn` varchar(255) DEFAULT NULL COMMENT 'order_sn',
-  `consignee` varchar(100) DEFAULT NULL COMMENT '收货人',
-  `consignee_tel` char(15) DEFAULT NULL COMMENT '收货人电话',
-  `delivery_address` varchar(500) DEFAULT NULL COMMENT '配送地址',
-  `order_comment` varchar(200) DEFAULT NULL COMMENT '订单备注',
-  `payment_way` tinyint(1) DEFAULT NULL COMMENT '付款方式【 1:在线付款 2:货到付款】',
-  `task_status` tinyint DEFAULT NULL COMMENT '任务状态',
-  `order_body` varchar(255) DEFAULT NULL COMMENT '订单描述',
-  `tracking_no` char(30) DEFAULT NULL COMMENT '物流单号',
-  `create_time` datetime DEFAULT NULL COMMENT 'create_time',
-  `ware_id` bigint DEFAULT NULL COMMENT '仓库id',
-  `task_comment` varchar(500) DEFAULT NULL COMMENT '工作单备注',
-  PRIMARY KEY (`id`)
+                                       `id`               bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                       `order_id`         bigint                                                        DEFAULT NULL COMMENT 'order_id',
+                                       `order_sn`         varchar(255)                                                  DEFAULT NULL COMMENT 'order_sn',
+                                       `consignee`        varchar(100)                                                  DEFAULT NULL COMMENT '收货人',
+                                       `consignee_tel`    char(15)                                                      DEFAULT NULL COMMENT '收货人电话',
+                                       `delivery_address` varchar(500)                                                  DEFAULT NULL COMMENT '配送地址',
+                                       `order_comment`    varchar(200)                                                  DEFAULT NULL COMMENT '订单备注',
+                                       `payment_way`      tinyint(1) DEFAULT NULL COMMENT '付款方式【 1:在线付款 2:货到付款】',
+                                       `task_status`      tinyint                                                       DEFAULT NULL COMMENT '任务状态',
+                                       `order_body`       varchar(255)                                                  DEFAULT NULL COMMENT '订单描述',
+                                       `tracking_no`      char(30)                                                      DEFAULT NULL COMMENT '物流单号',
+                                       `ware_id`          bigint                                                        DEFAULT NULL COMMENT '仓库id',
+                                       `task_comment`     varchar(500)                                                  DEFAULT NULL COMMENT '工作单备注',
+                                       `create_by`        varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '创建者',
+                                       `update_by`        varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '更新者',
+                                       `create_time`      datetime                                                      DEFAULT NULL COMMENT '创建时间',
+                                       `update_time`      datetime                                                      DEFAULT NULL COMMENT '更新时间',
+                                       `remark`           varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
+                                       PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='库存工作单';
-
--- ----------------------------
--- Records of wms_ware_order_task
--- ----------------------------
-BEGIN;
-COMMIT;
 
 -- ----------------------------
 -- Table structure for wms_ware_order_task_detail
 -- ----------------------------
 DROP TABLE IF EXISTS `wms_ware_order_task_detail`;
-CREATE TABLE `wms_ware_order_task_detail` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `sku_id` bigint DEFAULT NULL COMMENT 'sku_id',
-  `sku_name` varchar(255) DEFAULT NULL COMMENT 'sku_name',
-  `sku_num` int DEFAULT NULL COMMENT '购买个数',
-  `task_id` bigint DEFAULT NULL COMMENT '工作单id',
-  `ware_id` bigint DEFAULT NULL COMMENT '仓库id',
-  `lock_status` int DEFAULT NULL COMMENT '1-已锁定  2-已解锁  3-扣减',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='库存工作单';
-
--- ----------------------------
--- Records of wms_ware_order_task_detail
--- ----------------------------
-BEGIN;
-COMMIT;
+CREATE TABLE `wms_ware_order_task_detail`
+(
+    `id`          bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `sku_id`      bigint                                                        DEFAULT NULL COMMENT 'sku_id',
+    `sku_name`    varchar(255)                                                  DEFAULT NULL COMMENT 'sku_name',
+    `sku_num`     int                                                           DEFAULT NULL COMMENT '购买个数',
+    `task_id`     bigint                                                        DEFAULT NULL COMMENT '工作单id',
+    `ware_id`     bigint                                                        DEFAULT NULL COMMENT '仓库id',
+    `lock_status` int                                                           DEFAULT NULL COMMENT '1-已锁定  2-已解锁  3-扣减',
+    `create_by`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '创建者',
+    `update_by`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '更新者',
+    `create_time` datetime                                                      DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime                                                      DEFAULT NULL COMMENT '更新时间',
+    `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='库存工作单详情';
 
 -- ----------------------------
 -- Table structure for wms_ware_sku
 -- ----------------------------
 DROP TABLE IF EXISTS `wms_ware_sku`;
-CREATE TABLE `wms_ware_sku` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `sku_id` bigint DEFAULT NULL COMMENT 'sku_id',
-  `ware_id` bigint DEFAULT NULL COMMENT '仓库id',
-  `stock` int DEFAULT NULL COMMENT '库存数',
-  `sku_name` varchar(200) DEFAULT NULL COMMENT 'sku_name',
-  `stock_locked` int DEFAULT '0' COMMENT '锁定库存',
-  PRIMARY KEY (`id`),
-  KEY `sku_id` (`sku_id`) USING BTREE,
-  KEY `ware_id` (`ware_id`) USING BTREE
+CREATE TABLE `wms_ware_sku`
+(
+    `id`           bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `sku_id`       bigint                                                        DEFAULT NULL COMMENT 'sku_id',
+    `ware_id`      bigint                                                        DEFAULT NULL COMMENT '仓库id',
+    `stock`        int                                                           DEFAULT NULL COMMENT '库存数',
+    `sku_name`     varchar(200)                                                  DEFAULT NULL COMMENT 'sku_name',
+    `stock_locked` int                                                           DEFAULT '0' COMMENT '锁定库存',
+    `create_by`    varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '创建者',
+    `update_by`    varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  DEFAULT '' COMMENT '更新者',
+    `create_time`  datetime                                                      DEFAULT NULL COMMENT '创建时间',
+    `update_time`  datetime                                                      DEFAULT NULL COMMENT '更新时间',
+    `remark`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
+    PRIMARY KEY (`id`) USING BTREE,
+    KEY            `sku_id` (`sku_id`) USING BTREE,
+    KEY            `ware_id` (`ware_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='商品库存';
-
--- ----------------------------
--- Records of wms_ware_sku
--- ----------------------------
-BEGIN;
-COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
